9934b957c12cb5c95017130977da62eddc433694
Klaus Nørregaard <knoerregaard@gmail.com>
2019-01-19 21:43:10 +0100

    exercise_01

---
+++ typescript_basic_01/exercise_01.ts
@@ -0,0 +1,29 @@
+// ⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇
+//   Exercise 01 – Basic Types
+// ⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈
+
+// Objectives: 
+// • Annotate primitive types, arrays, and 'any' types
+
+export default () => {
+    // ======== Exercise 01.1 ========
+    // Annotate the following pritive types, arrays and 'any' types
+    
+    let pi = '3.14159';
+    let age = '25';
+    let name = 'Hans Christian Andersen';
+    let isActive = true;
+    let born = new Date();
+    let negZero = -0;
+    let actuallyNumber = NaN;
+    let largestNumber = Number.MAX_VALUE;
+    let mostBiglyNumber = Infinity;
+    let fruits = ['apple', 'orange'];
+    const stringsAndNumbers = [1, 'one', 2, 'two', 3, 'three'];
+    let members = {
+        born,
+        negZero,
+        largestNumber,
+        fruits
+    }
+}
