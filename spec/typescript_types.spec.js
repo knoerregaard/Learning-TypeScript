e1bd8dbdc0051927e803f5045b77271de0acb917
knoerregaard <knoerregaard@gmail.com>
2020-02-21 09:10:19 +0100

    update

--- spec/typescript_types.spec.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var exercise_01_1 = require("../typescript_types/exercise_01");
-describe('calculate', function () {
-    it('add', function () {
-        var result = exercise_01_1.types.Pi(3.13);
-        expect(result).toBe(3.13);
-    });
-});
